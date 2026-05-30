c542480b701f12e5c22202918d0cd9bce8d74f94
acadamex-final <acadamex@gmail.com>
2026-05-31 02:12:37 +0530

    Update supabase.ts

--- Acadamex-Final-main/src/services/supabase.ts
+++ Acadamex-Final-main/src/services/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://sjpaglxqoladgprpbwle.supabase.co',
-  'YOUR_SUPABASE_ANON_KEY'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqcGFnbHhxb2xhZGdwcnBid2xlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxNTg3NDUsImV4cCI6MjA5NTczNDc0NX0.b2p19F7djCBysdwk-UkT5sXvEiLdrsQNBSn8lFirolo'
 )
